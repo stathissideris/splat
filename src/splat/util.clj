9216fb30db6407d5151b23a6a6aeeadc1825c47f
Stathis Sideris <stathis@juxt.pro>
2016-01-23 03:30:39 +0000

    Simplified reading code

--- src/splat/util.clj
+++ src/splat/util.clj
@@ -1,24 +1,13 @@
 (ns splat.util
   (:require [clojure.java.io :as io]
-            [clojure.tools.reader :as reader]
-            [clojure.tools.reader.reader-types :as reader-types]
+            ;;[clojure.tools.reader :as reader]
+            ;;[clojure.tools.reader.reader-types :as reader-types]
+            [clojure.edn :as edn]
             [splat.ast :as ast]))
 
-(defn read-edn
-  [filename]
-  (with-open [rdr (io/reader (io/file filename))]
-    (binding [reader/*data-readers* {'float ast/float-literal
-                                     'long ast/long-literal}]
-     (let [rdr (reader-types/indexing-push-back-reader (java.io.PushbackReader. rdr))]
-       (try
-         (loop [out []]
-           (let [data (reader/read {:eof ::eof} rdr)]
-             (if (= ::eof data)
-               out
-               (recur (conj out data)))))
-         (catch Exception e
-           (when-let [{:keys [file line column] :as data} (ex-data e)]
-             (throw (ex-info (format "Error in '%s' line %d col %s: %s"
-                                     filename line column (.getMessage e))
-                             (assoc data :reason ::read-error)
-                             e)))))))))
+(defn read-edn [filename]
+  (edn/read-string
+   {:readers
+    {'float ast/->FloatLiteral
+     'long  ast/->LongLiteral}}
+   (str "(" (slurp filename) ")")))
