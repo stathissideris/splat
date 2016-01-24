77e926ee10261ceafa81a4d81fe7445d2fd9e80e
Stathis Sideris <stathis@juxt.pro>
2016-01-24 22:28:26 +0000

    Add unscrutify in the workflow to produce more readable C files

--- dev/dev.clj
+++ dev/dev.clj
@@ -16,11 +16,20 @@
 (def parse-source parser/parse-source)
 (def emit emitter/emit)
 
+(defn format-c-file [filename]
+  (-> (Runtime/getRuntime)
+      (.exec (into-array String ["/usr/local/bin/uncrustify"
+                                 "-c" "/Users/sideris/devel/splat/resources/uncrustify-config"
+                                 "--no-backup"
+                                 filename]))
+      .waitFor))
+
 (defn compile-all []
   (let [sources (fs/list-dir "resources/examples/")]
     (doseq [f sources]
       (print "Compiling" (fs/name f))
-      (println " -" (compile-file f "resources/target/")))))
+      (println " -" (compile-file f "resources/target/"))
+      (format-c-file (str "resources/target/" (fs/name f) ".c")))))
 
 (defn compile-example [s]
   (compile-file (str "resources/examples/" s) "resources/target/"))
